4a9f0cc29e195b4435bd9015bb29dbfa2fdc4607
csupreme19 <csupreme19@gmail.com>
2023-03-09 19:30:43 +0900

    외부 설정 주입 - @ConfigurationPropertiesScan 사용

--- src/main/java/hello/config/MyDataSourceConfigV1.java
+++ src/main/java/hello/config/MyDataSourceConfigV1.java
@@ -3,12 +3,10 @@ package hello.config;
 import hello.datasource.MyDataSource;
 import hello.datasource.MyDataSourcePropertiesV1;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @RequiredArgsConstructor
-@EnableConfigurationProperties(MyDataSourcePropertiesV1.class)
+//@EnableConfigurationProperties(MyDataSourcePropertiesV1.class)
 public class MyDataSourceConfigV1 {
 
     private final MyDataSourcePropertiesV1 properties;
